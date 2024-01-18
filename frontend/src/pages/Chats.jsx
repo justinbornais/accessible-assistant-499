0535b5a4c65431f93fa26c59c6a129f2e2e16f17
justinbornais <justin.bornais@gmail.com>
2024-01-18 13:11:37 -0500

    Updated more styles.

--- frontend/src/pages/Chats.jsx
+++ frontend/src/pages/Chats.jsx
@@ -28,16 +28,21 @@ export default function Chats() {
         {chatList.map(function (data) {
           return (
             <><div className="userMessage container d-flex flex-row justify-content-center">
-              <p>{data.userName}: </p><p>{data.userMsg}</p>
+              <p>{data.userName}:&nbsp;&nbsp;</p><p>{data.userMsg}</p>
             </div><div className="aiResponse container d-flex flex-row justify-content-center">
-                <p>ChatGPT: </p><p>{data.AIMsg}</p>
+                <p>ChatGPT:&nbsp;&nbsp;</p><p>{data.AIMsg}</p>
               </div></>
         )
       })}
       </div>
       <div className="chatbar container d-flex flex-row align-items-center justify-content-center">
             <div className="TextBox align-self-center p-2 w-30 mw-50" style={{height:"55px"}}>
-          <input value={question} onChange={handleChange}  className="w-100 h-100" name="userQuery" placeholder="What can I help you with?" style={{textAlignVertical:"top",borderColor:"#7da2a9",borderRadius:"10px"}}/>
+          <input value={question} onChange={handleChange}  className="w-100 h-100" name="userQuery" placeholder="What can I help you with?"
+            style={{
+              textAlignVertical: "top",
+              borderColor: "#7da2a9",
+              borderRadius: "10px"
+            }}/>
             </div>
             <div className="SubmitBtn align-self-center p-2">
                     <Button onClick={addChat}>
