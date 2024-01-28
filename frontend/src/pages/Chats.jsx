f8f3bc9239d49a870148f94a0be1ec553842325f
nboisclair64 <nboisclair64@gmail.com>
2024-01-28 14:07:11 -0500

    Added visualize button and layout for desktops. Need to fix size of visualize button. Potentially investigate if having the image appear under the ChatGPT response would be better for mobile and Desktop

--- frontend/src/pages/Chats.jsx
+++ frontend/src/pages/Chats.jsx
@@ -4,6 +4,13 @@ import { Button } from 'react-bootstrap';
 export default function Chats() {
   const [chatList, addChatEntry] = useState(JSON.parse(window.localStorage.getItem('chats')));
   const [question, setQuestion] = useState("");
+  const [imageEnabled, toggleImage] = useState("hidden");
+  const [imageSrc, changeImg] = useState("../../images/Robot.jpg");
+  const visualizeImage = {
+    visibility: imageEnabled,
+    
+  }
+  //To change image either change this value or u can overwrite the image it is pointing to
   const handleChange = event => {
     setQuestion(event.target.value);
   }
@@ -21,8 +28,32 @@ export default function Chats() {
     addChatEntry(JSON.parse(window.localStorage.getItem('chats'))); //Set current state to the new local storage list
     setQuestion("");
   }
+  const visualizeChat = value => () => {
+    if (value === "AIResponse") {
+      //Add code for calling backend and getting image
+      //Currently having static images being randomized
+      if (Math.floor(Math.random() * (3 - 1 + 1)) + 1 == 1) {
+        changeImg("../../images/Robot.jpg");
+      }
+      else if (Math.floor(Math.random() * (3 - 1 + 1)) + 1 == 2) {
+        changeImg("../../images/Robot2.jpg");
+      }
+      else if (Math.floor(Math.random() * (3 - 1 + 1)) + 1 == 3) {
+        changeImg("../../images/Robot3.jpg");
+      }
+        if (imageEnabled === "visible") {
+            toggleImage("hidden");
+        }
+        else {
+            toggleImage("visible");
+        }
+      }
+  }
   return (
     <>
+      <div className="mainScreen container d-flex flex-row justify-content-center">
+      <div className="colextra d-flex flex-column col-md-4 align-items-center"></div>
+        <div className="col1 d-flex flex-column col-md-4 align-items-center">
       <center><h2>Chat History</h2></center>
       <div className="allChats container d-flex flex-column align-items-center" style={{border:"10px", height:"380px",overflowX:"hidden", overflowY:"auto"}}>
         {chatList.map(function (data) {
@@ -30,11 +61,16 @@ export default function Chats() {
             <><div className="userMessage container d-flex flex-row justify-content-center">
               <p>{data.userName}:&nbsp;&nbsp;</p><p>{data.userMsg}</p>
             </div><div className="aiResponse container d-flex flex-row justify-content-center">
-                <p>ChatGPT:&nbsp;&nbsp;</p><p>{data.AIMsg}</p>
+                <p>ChatGPT:&nbsp;&nbsp;</p><p>{data.AIMsg}</p><Button className="visualizeButton" onClick={visualizeChat(data.AIMsg)}><img className="visualizeImg" src="../../images/visualize.png" alt="Visualize Button"></img></Button>
               </div></>
         )
       })}
-      </div>
+          </div>
+        </div>
+        <div className="col2 d-flex flex-column col-md-4 align-items-end">
+          <img className="visualizedImage" src={imageSrc} alt="Visualized Result" style={visualizeImage}></img>
+        </div>
+        </div>
       <div className="chatbar container d-flex flex-row align-items-center justify-content-center">
             <div className="TextBox align-self-center p-2 w-30 mw-50" style={{height:"55px"}}>
           <input value={question} onChange={handleChange}  className="w-100 h-100" name="userQuery" placeholder="What can I help you with?"
