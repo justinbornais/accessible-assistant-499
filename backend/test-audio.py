ca9ca87fb591e0736ef33787a2b05b8fc13372a3
Justin Bornais <justin.bornais@gmail.com>
2024-02-24 15:50:21 -0500

    Updated stuff.

--- backend/test-audio.py
+++ backend/test-audio.py
@@ -18,3 +18,7 @@ syn = Synthesizer(
 text = "Our whole universe was in a hot, dense state. Then nearly fourteen billion years ago expansion started, wait.... The earth began to cool, the autotrophs began to drool. Neanderthals developed tools. We built a wall (we built the pyramids). Math, science, history, unraveling the mysteries That all started with the big bang."
 outputs = syn.tts(text)
 syn.save_wav(outputs, f"audio-{model_name}.wav")
+
+text = "If there's a third world war, then all countries will go crazy and blow stuff up. It will be so serious that Joe Biden will even put down his chocolate chocolate-chip ice cream to give a press conference."
+outputs = syn.tts(text)
+syn.save_wav(outputs, f"audio-{model_name}2.wav")
