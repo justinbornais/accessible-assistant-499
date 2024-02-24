d55ffae7bdebd9b9ccfc5f72882cc5e7700affb1
Justin Bornais <justin.bornais@gmail.com>
2024-02-24 15:38:33 -0500

    Got TTS working with a basic script.

---
+++ backend/test-audio.py
@@ -0,0 +1,20 @@
+from TTS.utils.manage import ModelManager
+from TTS.utils.synthesizer import Synthesizer
+
+path = "./TTS/TTS/.models.json"
+model_manager = ModelManager(path)
+model_name = "speedy-speech"
+
+model_path, config_path, model_item = model_manager.download_model(f"tts_models/en/ljspeech/{model_name}")
+voc_path, voc_config_path, _ = model_manager.download_model(model_item["default_vocoder"])
+
+syn = Synthesizer(
+    tts_checkpoint=model_path,
+    tts_config_path=config_path,
+    vocoder_checkpoint=voc_path,
+    vocoder_config=voc_config_path
+)
+
+text = "Our whole universe was in a hot, dense state. Then nearly fourteen billion years ago expansion started, wait.... The earth began to cool, the autotrophs began to drool. Neanderthals developed tools. We built a wall (we built the pyramids). Math, science, history, unraveling the mysteries That all started with the big bang."
+outputs = syn.tts(text)
+syn.save_wav(outputs, f"audio-{model_name}.wav")
